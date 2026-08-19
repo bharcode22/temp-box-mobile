4ee1d628d2677e479622ae97d07f3b473daf3599
bharcode22 <bharcode22@gmail.com>
2026-08-19 22:34:55 +0800

    feat: custom [ci]

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import './global.css';
 console.log('📱 index.js is starting...');
 
 import { AppRegistry } from 'react-native';
+import { enableScreens } from 'react-native-screens';
+
+enableScreens(true);
+
 import App from './App';
 import { name as appName } from './app.json';
 
